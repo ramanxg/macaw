d0d268d4ac37c7550baba9652dc4c2b0d2800ecd
Raman Gupta <ramanxg@gmail.com>
2019-04-13 06:55:20 -0700

    moving towards using text

--- app/src/main/java/com/example/macaw/MainActivity.java
+++ app/src/main/java/com/example/macaw/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.macaw;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.hardware.Camera;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,30 +15,46 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int CAMERA_PIC_REQUEST = 1337;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private Bitmap currentImage;
+    private TextView tv;
+    Camera camera;
+    FrameLayout frameLayout;
+    ShowCamera showCamera;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
+        imageView = (ImageView)findViewById(R.id.imageView);
 
-        Button camerabtn = (Button) findViewById(R.id.camerabutton);
-        camerabtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dispatchTakePictureIntent();
-            }
-        });
+        camera = Camera.open();
+        showCamera = new ShowCamera(this, camera);
+        frameLayout.addView(showCamera);
+
         Button testbutton = (Button) findViewById(R.id.buttonTest);
         testbutton.setOnClickListener(new View.OnClickListener()
         {
@@ -49,25 +68,78 @@ public class MainActivity extends AppCompatActivity {
 
     private void labelImage()
     {
-        ImageView imageview = (ImageView) findViewById(R.id.ImageView01);
+        ImageView imageview = (ImageView) findViewById(R.id.imageView);
 
         FirebaseVisionImage image = imageFromBitmap(currentImage);
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                .getCloudTextRecognizer();
+        tv = findViewById(R.id.textView);
+
+        Task<FirebaseVisionText> result =
+                detector.processImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                tv.setText("Success!");
+                                processTextBlock(firebaseVisionText);
+
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        tv.setText("Unsuccessful");
+                                    }
+                                });
+
+
 
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    private void processTextBlock(FirebaseVisionText result) {
+        // [START mlkit_process_text_block]
+        String resultText = result.getText();
+        for (FirebaseVisionText.TextBlock block: result.getTextBlocks()) {
+            String blockText = block.getText();
+            Float blockConfidence = block.getConfidence();
+            List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+            Point[] blockCornerPoints = block.getCornerPoints();
+            Rect blockFrame = block.getBoundingBox();
+            for (FirebaseVisionText.Line line: block.getLines()) {
+                String lineText = line.getText();
+                Float lineConfidence = line.getConfidence();
+                List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+                Point[] lineCornerPoints = line.getCornerPoints();
+                Rect lineFrame = line.getBoundingBox();
+                for (FirebaseVisionText.Element element: line.getElements()) {
+                    String elementText = element.getText();
+                    Float elementConfidence = element.getConfidence();
+                    List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+                    Point[] elementCornerPoints = element.getCornerPoints();
+                    Rect elementFrame = element.getBoundingBox();
+                    System.out.println(elementText);
+                    TextView t = findViewById(R.id.textView);
+                    System.out.println(elementConfidence);
+                }
+            }
+        }
+        // [END mlkit_process_text_block]
+    }
+
+/*    private void dispatchTakePictureIntent() {
+
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-    }
+    }*/
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             currentImage = (Bitmap) extras.get("data");
 
-            ImageView imageview = (ImageView) findViewById(R.id.ImageView01); //sets imageview as the bitmap
+            ImageView imageview = (ImageView) findViewById(R.id.imageView); //sets imageview as the bitmap
             imageview.setImageBitmap(currentImage);
         }
     }
@@ -76,26 +148,4 @@ public class MainActivity extends AppCompatActivity {
         return FirebaseVisionImage.fromBitmap(bitmap);
         // [END image_from_bitmap]
     }
-    /**private boolean safeCameraOpen(int id) {
-        boolean qOpened = false;
-
-        try {
-            releaseCameraAndPreview();
-            camera = Camera.open(id);
-            qOpened = (camera != null);
-        } catch (Exception e) {
-            Log.e(getString(R.string.app_name), "failed to open Camera");
-            e.printStackTrace();
-        }
-
-        return qOpened;
-    }
-
-    private void releaseCameraAndPreview() {
-        preview.setCamera(null);
-        if (camera != null) {
-            camera.release();
-            camera = null;
-        }
-    }**/
 }
---
+++ app/src/main/java/com/example/macaw/ShowCamera.java
@@ -0,0 +1,95 @@
+package com.example.macaw;
+
+import android.content.Context;
+import android.content.res.Configuration;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback, Camera.PreviewCallback {
+
+    Camera camera;
+    SurfaceHolder holder;
+    private byte[] mPreviewFrameBuffer;
+    // Holds the current frame, so we can react on a click event:
+    private final Lock lock = new ReentrantLock();
+
+    public ShowCamera(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        holder = getHolder();
+        holder.addCallback(this);
+    }
+    @Override
+    public void onPreviewFrame(byte[] bytes, Camera camera) {
+        try {
+            lock.lock();
+            mPreviewFrameBuffer = bytes;
+        } finally {
+            lock.unlock();
+        }
+    }
+    public static Bitmap convertYuvByteArrayToBitmap(byte[] data, Camera camera) {
+        Camera.Parameters parameters = camera.getParameters();
+        Camera.Size size = parameters.getPreviewSize();
+        YuvImage image = new YuvImage(data, parameters.getPreviewFormat(), size.width, size.height, null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        image.compressToJpeg(new Rect(0, 0, size.width, size.height), 100, out);
+        byte[] imageBytes = out.toByteArray();
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        camera.setPreviewCallback(this);
+
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Camera.Parameters params = camera.getParameters();
+
+        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE)
+        {
+            params.set("orientation","portrait");
+            camera.setDisplayOrientation(90);
+            params.setRotation(90);
+        }
+        else
+        {
+            params.set("orientation","landscape");
+            camera.setDisplayOrientation(0);
+            params.setRotation(0);
+        }
+
+        params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+        camera.setParameters(params);
+
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
